02a15d47e5376a779fa93266e8a5e922db012625
AMESIME <obedamesime78@gmail.com>
2023-06-07 00:01:02 +0000

    Added the files

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,8 @@
+import express from 'express'
+const router = express.Router()
+import { authUser } from '../controllers/userControllers.js'
+
+
+router.post("/auth", authUser)
+
+export default router
